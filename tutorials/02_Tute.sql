565168686fd1edba7930daafd88d974923c6948f
Courtney Zhan <courtney@Courtneys-MBP.lan>
2021-08-20 13:43:36 +1000

    Tutorial 2 Q2 Courtney Solutions

--- tutorials/02_Tute.sql
+++ tutorials/02_Tute.sql
@@ -5,7 +5,7 @@ CREATE TABLE Emp (
     age INTEGER,
     salary REAL,
     -- Primary key goes here
-    
+    PRIMARY KEY (eid)
 );
 
 CREATE TABLE Dept (
@@ -14,9 +14,9 @@ CREATE TABLE Dept (
     budget REAL,
     managerid INTEGER,
     -- Primary key goes here
-    
+    PRIMARY KEY (did),
     -- Foreign key goes here
-    
+    FOREIGN KEY (managerid) REFERENCES Emp(eid)
     -- Foreign key option
 );
 
@@ -25,9 +25,10 @@ CREATE TABLE Works (
     did INTEGER,
     pcttime INTEGER,
     -- Primary key goes here
-    
+    PRIMARY KEY (eid, did), -- composite pk
     -- Foreign key goes here
-    
+    FOREIGN KEY (eid) REFERENCES Emp(eid),
+    FOREIGN KEY (did) REFERENCES Dept(did),
     -- Foreign key option
 );
 
@@ -36,12 +37,13 @@ CREATE TABLE Dept (
     did INTEGER,
     dname CHAR(20),
     budget REAL,
-    managerid INTEGER,
+    managerid INTEGER NOT NULL,
     -- Primary key goes here
-    
+    PRIMARY KEY (did),
     -- Foreign key goes here
-    
+    FOREIGN KEY (managerid) REFERENCES Emp(eid),
     -- Foreign key option
+	ON DELETE CASCAE,
 );
 
 -- Part D - What happens when Toy department is deleted (Part B not Part C)
@@ -49,7 +51,7 @@ DELETE FROM Dept
 WHERE dname = 'Toy';
 
 /*
-
+Answers in Tute solutions. :)
 */
 
 -- Part E - Emp Constraint minimum salary of 10,000
@@ -59,22 +61,39 @@ CREATE TABLE Emp (
     age INTEGER,
     salary REAL,
     -- Primary key goes here
-    
+    PRIMARY KEY (eid)
     -- Constraint goes here
-    
+    CHECK (salary >= 10000)
 );
 
 -- Part F - Assert all managers age > 30
 CREATE ASSERTION ManagerAge CHECK (
-    
+    NOT EXISTS (
+    	SELECT *
+		FROM Emp E, Dept D
+		WHERE E.eid = D.managerid --  managers only
+		AND E.age <= 30 -- can't be less than or eq 30
+    )
 );
 
 -- Part G - Assert total percentage of all appointments for an employee is under 100%
 CREATE ASSERTION Under100 CHECK (
-    
+    NOT EXISTS (
+    	SELECT * 
+		FROM Works W
+		GROUP BY eid
+		HAVING SUM(pcttime > 100) 
+    )
 );
 
 -- Part H - Assert a manager must always have a higher salary than any employee that they manage
 CREATE ASSERTION ManagerHigherSalary CHECK (
-    
+    NOT EXISTS (
+    	SELECT * 
+		FROM Emp E, Emp M, Dept D, Works W
+		WHERE E.eid = W.eid
+		AND D.did = W.did
+		AND D.managerid = M.eid
+		AND M.salary < E.salary
+    )
 );
